25e4fa3f2f0e04a09240eed20dc9937e98e7dd02
qasimnauman <m.qasimnauman@gmail.com>
2024-10-22 01:12:32 +0500

    updated the logpublish.sh file

--- logspublish.sh
+++ logspublish.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-CONTAINER_ID="65dc3c14c242"
+CONTAINER_NAME="my-container-name"
 LOG_DIR="/home/ubuntu/logs"
 S3_BUCKET="bucket-docker-ec2-rds-s3"
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-LOG_FILE="$LOG_DIR/my-container-log-$TIMESTAMP.log"
+LOG_FILE="$LOG_DIR/$CONTAINER_NAME-$TIMESTAMP.log"
 
 mkdir -p "$LOG_DIR"
 
-docker logs $CONTAINER_ID > "$LOG_FILE"
+# Get the exact container ID using the name
+CONTAINER_ID=$(sudo docker ps -qf "name=$CONTAINER_NAME")
 
-aws s3 cp "$LOG_FILE" "s3://$S3_BUCKET/container-logs/"
-
-# commands to run the cronjob
-# crontab -e
-# 0 0 * * * bash /home/ubuntu/scripts/logspublish.sh
+# Fetch the logs only if the container is running
+if [ -n "$CONTAINER_ID" ]; then
+    sudo docker logs "$CONTAINER_ID" > "$LOG_FILE"
+    aws s3 cp "$LOG_FILE" "s3://$S3_BUCKET/container-logs/"
+else
+    echo "Container $CONTAINER_NAME is not running."
+fi
